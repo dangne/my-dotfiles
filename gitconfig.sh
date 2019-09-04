94a4f972d22c0f5aa7c32d658b9e687a15511c06
dangne <dang.nguyen.bkit@gmail.com>
2019-09-04 10:45:37 +0700

    Refactor script

---
+++ gitconfig.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# Move .vimrc to home folder
+sudo mv .vimrc ~
+
+# Setup Vundle and Plugins
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+vim +PluginInstall +qall
+
+# Patch font for powerline
+sudo apt-get install -y fonts-powerline 
+
+# Define global variables
+echo "Input your Github username:"
+read username
+echo "Input your Github email:"
+read useremail
+git config --global user.name $username
+git config --global user.email $useremail
+git config --global core.excludesfile ~/.gitignore
+
+# Define global aliases
+git config --global alias.co checkout
+git config --global alias.br branch
+git config --global alias.cm commit
+git config --global alias.st status
