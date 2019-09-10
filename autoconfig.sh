321ab78bcbf055df5720325ce983be8039809991
dangne <dang.nguyen.bkit@gmail.com>
2019-09-11 00:44:43 +0700

    Add shell theme installation and add echoing every step

--- autoconfig.sh
+++ autoconfig.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 
-cd ~
-
-# Install Google Chrome
+echo "### Install Google Chrome"
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 sudo dpkg -i google-chrome-stable_current_amd64.deb
 
-# Install packages
+echo "### Install packages"
 sudo apt-get install -y vim 
 sudo apt-get install -y g++
 sudo apt-get install -y python3 
@@ -16,23 +14,31 @@ sudo apt-get install -y git
 sudo apt-get install -y ibus-unikey
 sudo apt-get install -y cmake
 
-# Setup unikey
-sensible-browser https://vinasupport.com/huong-dan-cai-bo-go-tieng-viet-ibus-unikey-tren-ubuntu/
+echo "### Setup unikey"
+google-chrome https://vinasupport.com/huong-dan-cai-bo-go-tieng-viet-ibus-unikey-tren-ubuntu/
 sudo apt-get update
 ibus restart
 
-# Setup theme
+echo "### Setup theme"
+google-chrome https://www.gnome-look.org/p/1166289/
+google-chrome https://www.gnome-look.org/p/1253385/
+google-chrome http://ubuntuhandbook.org/index.php/2017/05/enable-shell-theme-in-gnome-tweak-tool-in-ubuntu/
 sudo apt-get install -y papirus-icon-theme
 sudo apt-get install -y gnome-tweak-tool
 sudo add-apt-repository ppa:papirus/papirus
 sudo apt-get update
-sensible-browser https://www.gnome-look.org/p/1166289/
-sensible-browser https://www.gnome-look.org/p/1253385/
-gsettings set org.gnome.desktop.interface gtk-theme Sweet
-gsettings set org.gnome.desktop.wm.preferences theme Sweet
+echo "Download theme Sweet-Dark to Downloads to proceed..."
+read waiting
+tar -xf Sweet-Dark.tar.xz -C ~/.themes/
+firefox https://extensions.gnome.org
+echo "Click the link text “click here to install browser extension” and follow the pop-ups to install the browser extension"
+sudo apt install chrome-gnome-shell
+firefox https://extensions.gnome.org/extension/19/user-themes/
+gsettings set org.gnome.desktop.interface gtk-theme Sweet-Dark
+gsettings set org.gnome.desktop.wm.preferences theme Sweet-Dark
 
-# Config git
-source gitconfig.sh
+echo "### Config git"
+source ./gitconfig.sh
 
-# Apply changes
-source .bashrc
+echo "### Apply changes"
+source ~/.bashrc
--- gitconfig.sh
+++ gitconfig.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 
-# Move .vimrc to home folder
-sudo mv .vimrc ~
+echo "### Move .vimrc to home folder"
+sudo cp .vimrc ~
 
-# Setup Vundle and Plugins
+echo "### Setup Vundle and Plugins"
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-    # Install YouCompleteMe
+    echo "### Install YouCompleteMe"
     sudo apt install build-essential cmake python3-dev
     cd ~/.vim/bundle/YouCompleteMe
     python3 install.py
 vim +PluginInstall +qall
 
-# Patch font for powerline
+echo "### Patch font for powerline"
 sudo apt-get install -y fonts-powerline 
 
-# Define global variables
+echo "### Define global variables"
 echo "Input your Github username:"
 read username
 echo "Input your Github email:"
@@ -23,7 +23,7 @@ git config --global user.name $username
 git config --global user.email $useremail
 git config --global core.excludesfile ~/.gitignore
 
-# Define global aliases
+echo "### Define global aliases"
 git config --global alias.co checkout
 git config --global alias.br branch
 git config --global alias.cm commit
