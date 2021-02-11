81003b893e6ed1a75a2f29ef78e507b1bfcff9b3
dangne <dang.nguyen.bkit@gmail.com>
2021-02-11 21:33:15 +0700

    Refactor script

--- README.md
+++ README.md
@@ -1,23 +1,15 @@
-# My .dotfiles ![git](https://img.shields.io/badge/-git-success) ![vim](https://img.shields.io/badge/-vim-success) ![bash](https://img.shields.io/badge/-bash-success) ![tmux](https://img.shields.io/badge/-tmux-success) ![octave](https://img.shields.io/badge/-octave-success)
+# My dotfiles 
 
-These are all the configuration files that I made to make my tools more comfy, feel free to customize it as your own needs. I also wrote the `autoconfig.sh` to perform a quick setup whenever I move to a new development environment or reinstall my OS.
-
-### How to use autoconfig.sh
+### Usage
 
 Display help and all config options:
 
 ```
-bash autoconfig.sh
+./setup.sh help
 ```
 
 Config your selected option
 
 ```
-bash autoconfig.sh [<option>]
-```
-
-or config everything with
-
-```
-bash autoconfig.sh all
+./setup.sh bash vim
 ```
---
+++ setup.sh
@@ -0,0 +1,125 @@
+#!/bin/bash
+
+THIS_DIR=$(DIRNAME=$(dirname "$0"); cd "$DIRNAME"; pwd)
+THIS_FILE=$(basename "$0")
+THIS_PATH="$THIS_DIR/$THIS_FILE"
+USAGE="
+usage: setup.sh [options]
+
+Available options:
+
+bamboo        Install ibus-bamboo
+tmux          Install tmux
+typora        Install typora 
+zsh           Install zsh
+bash          Configure bash
+git           Configure git
+vim           Configure vim
+misc          Install miscellaneous packages
+"
+
+install_bamboo() {
+  # source: https://github.com/BambooEngine/ibus-bamboo
+  echo "Installing ibus-bamboo... "
+
+  sudo add-apt-repository ppa:bamboo-engine/ibus-bamboo
+  sudo apt-get update
+  sudo apt-get install -y ibus-bamboo
+  ibus restart
+}
+
+install_misc() {
+  echo "Installing miscellaneous packages..."
+
+  sudo apt-get install trash-cli
+}
+
+install_tmux() {
+  # source: https://github.com/tmux/tmux
+  echo "Installing tmux..."
+
+  sudo apt install -y tmux
+}
+
+install_typora() {
+  # source: https://typora.io/#linux
+  echo "Installing typora... "
+
+  wget -qO - https://typora.io/linux/public-key.asc | sudo apt-key add -
+
+  # add Typora's repository
+  sudo add-apt-repository 'deb https://typora.io/linux ./'
+  sudo apt-get update
+
+  # install typora
+  sudo apt-get install -y typora
+}
+
+install_zsh() {
+  echo "Installing zsh..."
+
+  sudo apt install -y zsh
+  sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+  echo "source ${THIS_DIR}/dotfiles/.zshrc" >> ~/.zshrc
+}
+
+config_bash() {
+  echo "Configuring bash..."
+
+  echo "source ${THIS_DIR}/dotfiles/.bashrc" >> ~/.bashrc
+  source ~/.bashrc
+}
+
+config_git() {
+  echo "Configuring git..."
+
+  sudo apt-get install -y git
+  read -p "Please enter your username:" username
+  read -p "Please enter your email:" email
+  git config --global user.name "${username}"
+  git config --global user.email "${email}"
+  git config --global core.excludesFile "${THIS_DIR}/dotfiles/.gitignore"
+  git config --global core.editor vim
+   
+  git config --global alias.co checkout
+  git config --global alias.br branch
+  git config --global alias.cm commit
+  git config --global alias.st status
+}
+
+config_vim() {
+  echo "Configuring vim..."
+
+  sudo apt-get install -y vim 
+  cp "${THIS_DIR}/dotfiles/.vimrc" ~
+  git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+  vim +PluginInstall +qall
+  sudo apt-get install -y fonts-powerline 
+  sudo apt install -y build-essential cmake python3-dev
+  cd ~/.vim/bundle/YouCompleteMe
+  python3 install.py
+}
+
+[[ $# -eq 0 ]] && h=true || h=false
+for opt in $@; do
+  if [[ "$opt" == "help" ]] ; then h=true ; break ; fi
+done
+
+if [[ "$h" = true ]] ; then
+  echo "$USAGE" ;
+else
+  while [[ $# -gt 0 ]] ; do
+    case "$1" in 
+      bamboo) install_bamboo ;;
+      misc)   install_misc ;;
+      tmux)   install_tmux ;;
+      typora) install_typora ;;
+      zsh)    install_zsh ;;
+      bash)   config_bash ;;
+      git)    config_git ;;
+      vim)    config_vim ;;
+      *)      echo "Unrecognized option: $1" ;;
+    esac
+    shift
+  done
+fi
